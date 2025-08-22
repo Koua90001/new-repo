897c850a0192e0f6aed5f23915d66eaa84083e92
Koua90001 <oumarkouakou@yahoo.com>
2025-08-22 02:31:44 -0400

    feat(server): Stage 2 backend (auth, validation, errors, logging, rate limit, ESLint)

--- server/db.js
+++ server/db.js
@@ -4,6 +4,6 @@ const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/final_project'
 
 mongoose.connect(uri)
   .then(() => console.log('Mongo connected'))
-  .catch((e) => console.error('Mongo connection error', e));
+  .catch(e => console.error('Mongo connection error', e));
 
 module.exports = mongoose;
